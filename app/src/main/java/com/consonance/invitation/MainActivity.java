9b4886c402a29f0ca57c11765e1e88fa8741e6e5
langrenbule <546024423@qq.com>
2016-06-05 23:41:59 +0800

    (*)一些必须使用的UI界面

--- app/src/main/java/com/consonance/invitation/MainActivity.java
+++ app/src/main/java/com/consonance/invitation/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,20 +17,26 @@ import android.view.MenuItem;
 
 import com.consonance.invitation.adapter.OrderAdapter;
 import com.consonance.invitation.test.MonitorData;
+import com.deity.customview.widget.NavigationBar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,View.OnClickListener, ViewPager.OnPageChangeListener{
 //    @BindView(R.id.order_list)
     private static final int REFRESH_COMPLETE = 0X110;
     public RecyclerView mRecyclerView;
     private OrderAdapter mAdapter;
     private SwipeRefreshLayout mSwipeLayout;
-
+    //
+    private ViewPager mViewPager;
+    private List<Fragment> mFragmentList;
+    private List<NavigationBar> mNavigationBarList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +47,38 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
         initRecyclerView();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initView();
+//        pagerAdapter();
+        initEvent();
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        mNavigationBarList.add((NavigationBar)findViewById(R.id.tab_first));
+        mNavigationBarList.add((NavigationBar)findViewById(R.id.tab_second));
+        mNavigationBarList.add((NavigationBar)findViewById(R.id.tab_third));
+        mNavigationBarList.add((NavigationBar)findViewById(R.id.tab_fourth));
+
+        mNavigationBarList.get(0).setAlpha(1.0f);
+
+    }
+
+//    private void pagerAdapter() {
+//        mFragmentList = new ArrayList<android.support.v4.app.Fragment>();
+//        for (int i=0;i<4;i++){
+//            mFragmentList.add(new Fragment(i));
+//        }
+//        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),mFragmentList);
+//        mViewPager.setAdapter(adapter);
+//    }
+
+    private void initEvent() {
+        for(int i = 0; i < mNavigationBarList.size(); i++){
+            mNavigationBarList.get(i).setOnClickListener(this);
+            mNavigationBarList.get(i).setTag(i);
+        }
+        mViewPager.addOnPageChangeListener(this);
     }
 
     public void initRecyclerView(){
@@ -101,6 +133,38 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     public void onRefresh() {
         //请求网络,这边我模拟耗时
         mHandler.sendEmptyMessageDelayed(REFRESH_COMPLETE, 2000);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int number = (Integer) v.getTag();
+        changeAlpha(number);
+        mViewPager.setCurrentItem(number, false);
+    }
+
+    public void changeAlpha(int number){
+        for (NavigationBar btn:mNavigationBarList){
+            btn.setAlpha(0f);
+        }
+        mNavigationBarList.get(number).setAlpha(1.0f);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (positionOffsetPixels != 0){
+            mNavigationBarList.get(position).setAlpha(1 - positionOffset);
+            mNavigationBarList.get(position+1).setAlpha(positionOffset);
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 }
